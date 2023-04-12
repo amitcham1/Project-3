4c41c7d1fa67ba9951503a724f6b018100023caa
BatchOfAllTrades <mbacheld@msudenver.edu>
2023-04-12 01:07:16 -0600

    Updated problem 1 to "return" a result and save to a variable which is then printed to the screen. Much better! Might be able to use it for subsequent problems!

--- Project3.lisp
+++ Project3.lisp
@@ -1,15 +1,37 @@
 ; Problem 1
 
-(defun set-member (a_list item)
+(defun set-member (set item)
     (COND
-        ((EQ item (CAR a_list)) (WRITE-LINE "T"))
-        ((NULL (CAR a_list)) (WRITE-LINE "NIL"))
-        ((NOT(EQ item (CAR a_list))) (set-member (CDR a_list) item))
+        ((EQ item (CAR set)) T)
+        ((NULL (CAR set)) NIL)
+        ((NOT(EQ item (CAR set))) 
+            (set-member (CDR set) item)
+        )
     )
 )
 
-(set-member '(1 2 3) 3)
-(set-member '(1 2 3) 4)
+(SETQ result (set-member '(1 2 3) 3))
+(WRITE result)
+(TERPRI)
+(SETQ result (set-member '(1 2 3) 4))
+(WRITE result)
+(TERPRI)
 
-; Problem 2
+; Problem 2 NOT WORKING YET
 
+(defun set-union (set-1 set-2)
+    (COND  
+        ((NULL set-1) set-2)
+        ((NULL set-2) set-1)
+        ((EQ (CAR set-1) (CAR set-2))
+            (set-union (CDR set-1) set-2)
+        )
+        ((NOT(EQ (CAR set-1) (CAR set-2))) 
+            (set-union set-1 (CDR set-2))
+        )
+    )
+)
+
+(setq result (set-union '(1 2) '(2 4)))
+(WRITE result)
+(TERPRI)
