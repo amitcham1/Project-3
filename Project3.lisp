a7fd113b6e68893dedd3fac96d5664daad293047
BatchOfAllTrades <mbacheld@msudenver.edu>
2023-04-12 00:09:21 -0600

    Problem 1 updated function name to match assignment example

--- Project3.lisp
+++ Project3.lisp
@@ -1,10 +1,15 @@
-(defun list-member (a_list item)
+; Problem 1
+
+(defun set-member (a_list item)
     (COND
         ((EQ item (CAR a_list)) (WRITE-LINE "T"))
         ((NULL (CAR a_list)) (WRITE-LINE "NIL"))
-        ((NOT(EQ item (CAR a_list))) (list-member (CDR a_list) item))
+        ((NOT(EQ item (CAR a_list))) (set-member (CDR a_list) item))
     )
 )
 
-(list-member '(1 2 3) 3)
-(list-member '(1 2 3) 4)
+(set-member '(1 2 3) 3)
+(set-member '(1 2 3) 4)
+
+; Problem 2
+
